c251486d699b603dc809b77304e71da73cb650ff
James Treasure <jandrewtreasure@gmail.com>
2017-02-12 18:19:24 +0000

    Trying to setup github pages

--- Code/game.js
+++ Code/game.js
@@ -422,7 +422,7 @@ $(document).ready(function () {
             async: false
         });
 
-        $.getJSON("json/settings.json", function (json) {
+        $.getJSON("settings.json", function (json) {
             level = json["level" + levelNumber];
         });
     }
